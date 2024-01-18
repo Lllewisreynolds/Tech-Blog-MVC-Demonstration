70edf062e2cb9b001074efe64388f66cbd0ae16f
Lllewisreynolds <lsreynolds108@gmail.com>
2024-01-18 00:15:27 +0000

    "Models folder populated. Associations between 3 models created in index.js to establish relationship between users, blogs and comments in database. Models created for each of our necessary tables within database, including primary & foreign key relationships. User model contains added functionality through use of bcrypt package to allow access to further authentication and password hashing"

--- models/index.js
+++ models/index.js
@@ -1,1 +1,36 @@
-// 
+const User = require('./user');
+const Blog = require('./blog');
+const Comment = require('./comment');
+
+//Creating associations between the above models
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Blog, Comment };
--- models/user.js
+++ models/user.js
@@ -1,1 +1,59 @@
-// 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    // function uses bcrypt method to check password provided at login against stored password that has been hashed with bcrypt package 
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+
+    {
+        hooks: {
+            // hook serves to hash password before storing it in database
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // same as above but for existing user's password
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
