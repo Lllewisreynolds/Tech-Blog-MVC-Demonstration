81b326df21c07a6ec8337666e03304557c013c3c
Lllewisreynolds <lsreynolds108@gmail.com>
2024-01-18 12:17:32 +0000

    "Established essential routes for initial user experience when opening the application to home page (serving up blog posts, managing login and protecting dashboard behind said login). Handlebars templating to be handled on front-end later once back-end fully tested through insomnia after all routes established"

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,1 +1,90 @@
-// 
+const router = require('express').Router();
+const { Blog, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+//  Establishing the essential routes for initial user experience when opening the application:
+
+// main homepage: (/) 
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['userName'],
+                },
+
+            ],
+        });
+
+        // Serialises data in order for the template to parse it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// individual blog posts (/blogs/:id)
+router.get('/blogs/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['userName'],
+                },
+                {
+                    model: Comment,
+                }
+            ],
+        });
+
+        const blog = blogData.get({ plain: true });
+        // console.log(blog);
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// withAuth middleware used as a gatekeeper as a pre-requisite to access protected dashboard route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// and login (/login)
+router.get('/login', (req, res) => {
+
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
