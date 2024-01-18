b194139ff648e91c71482ea30cb5f2c8251f94f0
Lllewisreynolds <lsreynolds108@gmail.com>
2024-01-18 11:39:25 +0000

    "Index.js codified within Controllers to establish and direct traffic along appropriate routes"

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,12 @@
-// 
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes')
+
+// Standard directing of incoming requests to appropriate controllers based on their URL pattern
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+module.exports = router;
